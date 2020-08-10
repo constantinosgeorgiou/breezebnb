e8fbb26d9dd5fc3b615e0197c9971edf2e4c85fa
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2020-08-10 15:39:57 +0300

    add table creation and deletion

--- database/config.sql
+++ database/config.sql
@@ -11,6 +11,12 @@
 --
 --
 -- ------------------------------
+-- EXTENSIONS
+-- ------------------------------
+CREATE EXTENSION
+IF NOT EXISTS "uuid-ossp";
+
+-- ------------------------------
 -- CREATE TYPES
 -- ------------------------------
 DROP TYPE IF EXISTS user_role;
@@ -27,9 +33,42 @@ DROP TABLE IF EXISTS reviews;
 -- ------------------------------
 -- CREATE TYPES
 -- ------------------------------
-CREATE TYPE user_role AS ENUM (
+CREATE TYPE user_role AS ENUM
+(
     'admin',
     'guest',
     'host',
     'ghost' -- guest AND host
+);
+
+-- ------------------------------
+-- CREATE TABLES
+-- ------------------------------
+CREATE TABLE users
+(
+    user_id UUID DEFAULT uuid_generate_v4 (),
+    user_name VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    phone VARCHAR,
+    user_role USER_ROLE NOT NULL,
+    picture VARCHAR,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE listings
+(
+    listing_id UUID DEFAULT uuid_generate_v4 (),
+    listing_name VARCHAR NOT NULL,
+    cost VARCHAR NOT NULL,
+    listing_type VARCHAR NOT NULL,
+    listing_location VARCHAR NOT NULL,
+    picture VARCHAR
+);
+
+CREATE TABLE user_has_listings
+(
+    listing_owner UUID REFERENCES users(user_id)
 );
