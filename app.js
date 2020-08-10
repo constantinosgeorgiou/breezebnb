c225770c8e46a687e531dfdc330048587c6c9c4b
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2020-08-10 15:41:48 +0300

    add PORT variable from enviroment variable

--- app.js
+++ app.js
@@ -6,19 +6,17 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-console.log(process.env.PORT);
-
 app.get("/", (request, response) => {
     response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
 // ROUTES
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`);
 });
