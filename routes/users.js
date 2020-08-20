b7b2f386902fe4ff4fc18e8300b4a2d70ce3fa73
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2020-08-20 13:35:35 +0300

    add admin, hosting routes, rename User to user

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../controllers/users");
+const user = require("../controllers/users");
 
 // Find all users
-router.get("/", User.findAllUsers);
+router.get("/", user.findAllUsers);
 
 // Find user by ID
-router.get("/:userName", User.findUserByUserName);
+router.get("/:userName", user.findUserByUserName);
 
 // Create user
-router.post("/", User.createUser);
+router.post("/", user.createUser);
 
 // Update user
-router.put("/:userName", User.updateUserByUserName);
+router.put("/:userName", user.updateUserByUserName);
 
 // Delete user
-router.delete("/:userName", User.deleteUserByUserName);
+router.delete("/:userName", user.deleteUserByUserName);
 
 module.exports = router;
