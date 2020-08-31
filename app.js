aa6410c5eebd78da4caa3c0d11e364f3d9afecaa
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2020-08-31 15:16:02 +0300

    add cors and helmet for protection

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors"); // Used for Cross-origin resource sharing
+const helmet = require("helmet"); // Used as protection from some well-known web vulnerabilities by setting HTTP headers appropriately
+
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
