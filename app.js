48b41ee4f59f04e478a9660a9e29799f47c4bcfe
leonidasefrem <leonidasefrem@gmail.com>
2020-09-07 20:55:14 +0300

    add reviews routes

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.use(bodyParser.json());
 const IndexRoutes = require("./routes/index.js");
 const UsersRoutes = require("./routes/users.js");
 const ListingsRoutes = require("./routes/listings.js");
-// const ReviewsRoutes = require("./routes/reviews.js");
+const ReviewsRoutes = require("./routes/reviews.js");
 
 // Using routes
 app.use("/", IndexRoutes);
 app.use("/users", UsersRoutes);
 app.use("/listings", ListingsRoutes);
-// app.use("", ReviewsRoutes);
+app.use("/reviews", ReviewsRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}.`);
