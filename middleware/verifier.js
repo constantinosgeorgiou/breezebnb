9dad96d3b5bbf174e498ffa142a78ea5dd8cc852
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-11 00:33:48 +0300

    add isRoleValid, isRoleNotAdmin

--- middleware/verifier.js
+++ middleware/verifier.js
@@ -1,4 +1,5 @@
 const database = require("../database/index");
+const { response } = require("express");
 
 // Check for duplicate usernames
 const isUsernameUnique = (request, response, next) => {
@@ -26,7 +27,7 @@ const isUsernameUnique = (request, response, next) => {
                 });
             } else {
                 // Unique username
-                next();
+                next(); // SUCCESS
             }
         }
     );
@@ -58,13 +59,62 @@ const isEmailUnique = (request, response, next) => {
                 });
             } else {
                 // Unique email
-                next();
+                next(); // SUCCESS
             }
         }
     );
 };
 
+// Check for valid role
+const isRoleValid = (request, response, next) => {
+    const { userRole } = request.body;
+
+    // Check if role exists in database
+    database.query(
+        "SELECT $1::name = any(enum_range(null::user_role)::name[]) AS answer;",
+        // Documentation for query:
+        //  - https://www.postgresql.org/docs/current/functions-enum.html
+        //  - https://www.postgresql.org/docs/current/functions-comparisons.html#id-1.5.8.28.16
+        [userRole],
+        (error, results) => {
+            if (error) {
+                response.status(error.status || 500).json({
+                    error: {
+                        message: error.message,
+                    },
+                });
+            } else if (results.rows[0].answer === false) {
+                // Role doesn't exist
+                response.status(400).json({ message: "Role is not valid" });
+            } else {
+                // Role exists
+                next(); // SUCCESS
+            }
+        }
+    );
+};
+
+// Check if given user role is not admin
+const isRoleNotAdmin = (request, response, next) => {
+    const { userRole } = request.body;
+
+    if (userRole === "admin") {
+        // Role is admin
+
+        // Anauthorised request to create an admin user.
+        // Administrator profiles are created directly within the database.
+        response
+            .status(401)
+            .json({ message: "You are not allowed to do that." });
+    } else {
+        // Role is NOT admin
+        next(); // SUCCESS
+    }
+};
+
 module.exports = {
     isUsernameUnique,
     isEmailUnique,
+    isRoleValid,
+    isRoleNotAdmin,
 };
