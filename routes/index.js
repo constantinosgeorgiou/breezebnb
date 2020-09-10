ba4149d93dc9819af5ca2cfac6679213a01f838b
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-11 00:34:34 +0300

    Complete sign up route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 const Users = require("../controllers/users");
 
+const verifier = require("../middleware/verifier");
+
 router.get("/", (request, response) => {
     response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-router.post("/signup", Users.createUser);
+router.post(
+    "/auth/signup",
+    [
+        verifier.isUsernameUnique,
+        verifier.isEmailUnique,
+        verifier.isRoleValid,
+        verifier.isRoleNotAdmin,
+    ],
+    Users.createUser
+);
 
 // router.post("/signin", Users.)
 
