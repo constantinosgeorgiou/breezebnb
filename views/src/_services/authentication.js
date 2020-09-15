19105130c03a4a6b0858065d3c5dd206468a0e5b
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-15 16:54:33 +0300

    start work on authentication

--- views/src/_services/authentication.js
+++ views/src/_services/authentication.js
@@ -2,29 +2,38 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000";
 
-export function signin(data) {
+// Make a POST request to the server with username and password.
+// Store user information to local storage
+// Store JWT to local storage
+export function signin(username, password) {
     return axios
         .post(`${BASE_URL}/auth/signin`, {
-            userName: data.userName,
-            password: data.password,
+            userName: username,
+            password: password,
         })
         .then((response) => {
             console.log("Axios sign in: ", response);
 
-            localStorage.setItem("x-access-token", response.data.token);
-            localStorage.setItem(
-                "x-access-token-expiration",
-                Date.now() + 2 * 60 * 60 * 1000
-            );
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+
+            // localStorage.setItem("x-access-token", response.data.token);
+            // localStorage.setItem(
+            //     "x-access-token-expiration",
+            //     Date.now() + 2 * 60 * 60 * 1000
+            // );
             return response.data;
         })
         .catch((error) => Promise.reject("Authentication failed."));
 }
 
-export function signout(data){
-    return axios.post(`${BASE_URL}/users/${data.user.userName}/signout`).then((response)=>{
-        
-    })
+// Make a POST request to the server
+// Remove JWT from local storage
+export function signout(data) {
+    return axios
+        .post(`${BASE_URL}/users/${data.user.userName}/signout`)
+        .then((response) => {});
 }
 
 export function isAuthenticated() {
