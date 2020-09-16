a96cfce93dafd90072282db50b2eb4019f0ca6ac
leonidasefrem <leonidasefrem@gmail.com>
2020-09-16 19:11:05 +0300

    photo upload backend for listings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cloudinary = require('cloudinary').v2
 const bodyParser = require("body-parser"); // Incoming request body parsing middleware
 const cors = require("cors"); // Used for Cross-origin resource sharing
 const helmet = require("helmet"); // Used as protection from some well-known web vulnerabilities by setting HTTP headers appropriately
@@ -34,7 +35,7 @@ const ListingsRoutes = require("./routes/listings.js");
 const ReviewsRoutes = require("./routes/reviews.js");
 const MessagesRoutes = require("./routes/messages.js");
 const AdminRoutes = require("./routes/admin.js");
-
+const PhotosRoutes = require("./routes/photos.js");
 
 // Using routes
 app.use("/", IndexRoutes);
@@ -43,8 +44,17 @@ app.use("/listings", ListingsRoutes);
 app.use("/reviews", ReviewsRoutes);
 app.use("/messages", MessagesRoutes);
 app.use("/admin", AdminRoutes);
-
+app.use("/photos", PhotosRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}.`);
 });
+
+
+// cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+  });
+  
