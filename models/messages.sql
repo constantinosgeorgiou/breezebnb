481f0840783436d263fa27cd6d90525884ce5773
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-22 16:11:55 +0300

    add descriptive name to time, change to created

--- models/messages.sql
+++ models/messages.sql
@@ -6,7 +6,8 @@ CREATE TABLE Messages
     sender_id UUID REFERENCES users(user_id) NOT NULL,
     receiver_id UUID REFERENCES users(user_id) NOT NULL,
     listing_id UUID REFERENCES listings(listing_id) NOT NULL,
-    description character varying(500),
-    time timestamp without time zone NOT NULL,
-    PRIMARY KEY (message_id)
+    description CHARACTER VARYING(500),
+    created TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY
+    (message_id)
 );
