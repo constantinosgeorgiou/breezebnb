0c7fee578dc45947845c36d4234317ba427dfe49
leonidasefrem <leonidasefrem@gmail.com>
2020-09-22 11:46:59 +0300

    openstreetamaps functionlity backend post get req

---
+++ models/maps.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS listing_space;
+
+CREATE TABLE coordinates
+(
+    listing_id UUID NOT NULL REFERENCES listings(listing_id),
+    latitude double precision NOT NULL,
+    longitude double precision NOT NULL
+)
