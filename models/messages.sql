ff0b7cb89add1d9eeb0f1bf8eee6cda5d204ae6b
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-22 16:09:14 +0300

    make sender_id, receiver_id, listing_id reference respective tables

--- models/messages.sql
+++ models/messages.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS Messages;
 CREATE TABLE Messages
 (
     message_id UUID DEFAULT uuid_generate_v4 (),
-    sender_id character varying(37) NOT NULL,
-    receiver_id character varying(37) NOT NULL,
-    listing_id character varying(37) NOT NULL,
+    sender_id UUID REFERENCES users(user_id) NOT NULL,
+    receiver_id UUID REFERENCES users(user_id) NOT NULL,
+    listing_id UUID REFERENCES listings(listing_id) NOT NULL,
     description character varying(500),
     time timestamp without time zone NOT NULL,
     PRIMARY KEY (message_id)
