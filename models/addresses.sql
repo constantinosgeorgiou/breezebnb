e866a1f0c6ce2d0ab0717f5cb0583f973604dce7
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-22 16:24:30 +0300

    change order of fields

--- models/addresses.sql
+++ models/addresses.sql
@@ -4,10 +4,10 @@ CREATE TABLE addresses
 (
     address_id UUID DEFAULT uuid_generate_v4 (),
     country character varying(60) NOT NULL,
+    state character varying(60) NOT NULL,
+    city character varying(60) NOT NULL,
+    zip_code character varying(10) NOT NULL,
     street_address character varying(120) NOT NULL,
     apartment_number character varying(10) NOT NULL,
-    city character varying(60) NOT NULL,
-    state character varying(60) NOT NULL,
-    zip_code character varying(10) NOT NULL,
     PRIMARY KEY (address_id)
 );
