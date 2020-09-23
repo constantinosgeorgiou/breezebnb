59d6d5ea8c434b28f3d93e9f467823280937581d
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-23 16:04:06 +0300

    add user context to show / hide nav items

--- views/src/_components/Navigation.js
+++ views/src/_components/Navigation.js
@@ -1,26 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+
+import { getCurrentUser } from "../_services/user";
 
 import { Navbar, Nav, Container } from "react-bootstrap";
+import UserContext from "../_helpers/UserContext";
 
 class Navigation extends Component {
     render() {
         return (
-            <Navbar collapseOnSelect expand="lg">
-                <Container fluid>
-                    <Navbar.Brand href="/">BreezeBnB</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto"></Nav>
-                        <Nav>
-                            <Nav.Link href="/hosting">Become a host</Nav.Link>
-                            <Nav.Link href="/signin">Sign In</Nav.Link>
-                            <Nav.Link href="/signup">Sign Up</Nav.Link>
-                            <Nav.Link href="/profile">Profile</Nav.Link>
-                            <Nav.Link href="/host">host Dashboard</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <UserContext.Consumer>
+                {(context) => (
+                    <Navbar collapseOnSelect expand="lg">
+                        {console.log("context: ", context)}
+                        <Container fluid>
+                            <Navbar.Brand href="/">BreezeBnB</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="mr-auto"></Nav>
+                                <Nav>
+                                    {!context.user ? (
+                                        <Nav.Link href="/hosting">
+                                            Become a host
+                                        </Nav.Link>
+                                    ) : (
+                                        <Nav.Link href="/host">
+                                            Hostiting
+                                        </Nav.Link>
+                                    )}
+                                    {!context.user ? (
+                                        <Fragment>
+                                            <Nav.Link href="/signin">
+                                                Sign In
+                                            </Nav.Link>
+                                            <Nav.Link href="/signup">
+                                                Sign Up
+                                            </Nav.Link>
+                                        </Fragment>
+                                    ) : (
+                                        <Nav.Link href="/profile">
+                                            Profile
+                                        </Nav.Link>
+                                    )}
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                )}
+            </UserContext.Consumer>
         );
     }
 }
