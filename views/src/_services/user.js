1c5ccdca13201dae890804490ba6c03adb55ebc6
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-23 13:36:19 +0300

    add getReceivedReviews

--- views/src/_services/user.js
+++ views/src/_services/user.js
@@ -4,15 +4,8 @@ import authorizationHeader from "../_helpers/AuthorizationHeader";
 const API_URL = "http://localhost:5000";
 
 export function getReceivedReviews(userName) {
-    console.log("get reviews!!!");
-    return axios
-        .get(API_URL + "/users/" + userName + "/received-reviews", {
-            headers: authorizationHeader(),
-        })
-        .then((response) => {
-            console.log("response: ", response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    console.log(userName + "get request");
+    return axios.get(`${API_URL}/users/${userName}/reviews-received`, {
+        headers: authorizationHeader(),
+    });
 }
