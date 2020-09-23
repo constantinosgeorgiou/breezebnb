bf4aec9593a164874a7dc6b4b2efbb61cd2e7972
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-23 16:06:31 +0300

    add getCurrentUser

--- views/src/_services/user.js
+++ views/src/_services/user.js
@@ -3,6 +3,10 @@ import authorizationHeader from "../_helpers/AuthorizationHeader";
 
 const API_URL = "http://localhost:5000";
 
+export function getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+}
+
 export function getReceivedReviews(userName) {
     return axios.get(`${API_URL}/users/${userName}/reviews-received`, {
         headers: authorizationHeader(),
