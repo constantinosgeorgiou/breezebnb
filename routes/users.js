de2bd45b093873f3483828ab683e90ea878d57d2
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-23 13:32:22 +0300

    add received reviews route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("../controllers/users");
+const Reviews = require("../controllers/reviews");
 
 const { isAuthenticated } = require("../middleware/authentication");
 
@@ -10,7 +11,7 @@ const { isAdmin } = require("../middleware/authorization");
 router.get("/", [isAuthenticated, isAdmin], Users.retrieveUsers);
 
 // Find user by ID
-router.get("/:userName", Users.retrieveUserByUserName);
+router.get("/:userName", isAuthenticated, Users.retrieveUserByUserName);
 
 // Find first name and last name by ID
 router.get("/name/:user_id", Users.retrieveUserNameByUserId);
@@ -20,5 +21,12 @@ router.put("/:userName", isAuthenticated, Users.updateUserByUserName);
 
 // Delete user
 router.delete("/:userName", Users.deleteUserByUserName);
+
+// Retrieve all reviews directed to user
+router.get(
+    "/:userName/reviews-received",
+    isAuthenticated,
+    Reviews.retrieveReceivedReviews
+);
 
 module.exports = router;
