110ffcf396777ad4547cc9bf483c0600ec827971
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-23 13:32:53 +0300

    add isAuthenticated middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Reviews = require("../controllers/reviews");
 
+const { isAuthenticated } = require("../middleware/authentication");
+
+// TODO: Retrieve all reviews of user
+// router.get("/users/:userName/reviews")
 
 // find all reviews by review id
 router.get("/:review_id", Reviews.retrieveReviewByReviewId);
@@ -15,5 +19,4 @@ router.put("/:review_id", Reviews.updateReview);
 // Delete review
 router.delete("/:review_id", Reviews.deleteReview);
 
-
-module.exports = router; 
+module.exports = router;
