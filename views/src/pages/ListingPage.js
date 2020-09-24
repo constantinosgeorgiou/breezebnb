113f6d1be143889ca1445fe6be8d5451cf9d72d6
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-25 01:06:53 +0300

    beautify

--- views/src/pages/ListingPage.js
+++ views/src/pages/ListingPage.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import UserContext from "../_helpers/UserContext";
 
 import { getListingInformation } from "../_services/listings";
 
-import { BiStar } from "react-icons/bi";
+import { BiStar, BiBed, BiBath } from "react-icons/bi";
 
 import { Carousel } from "react-bootstrap";
 
@@ -18,13 +18,19 @@ class ListingPage extends Component {
             listing: {
                 id: faker.random.uuid(),
                 title: "Cozy apartment",
-                description: faker.lorem.paragraph(),
+                description: faker.lorem.paragraphs(3),
                 propertyType: "Bed and Breakfast",
                 guests: 3,
                 minimumBookingDays: 2,
                 rating: 5,
-
-                owner: 0,
+                cost: 15,
+                owner: {
+                    id: faker.random.uuid(),
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.lastName(),
+                    photo: faker.internet.avatar(),
+                    joined: faker.date.future(),
+                },
                 photos: [
                     faker.image.image(),
                     faker.image.image(),
@@ -113,39 +119,41 @@ class ListingPage extends Component {
     static contextType = UserContext;
 
     componentDidMount() {
-        // let listing = {};
-        // // Fetch listing info
+        // Fetch listing info
         getListingInformation(this.state.listingId)
             .then((response) => {
+                const { data } = response;
+
                 console.log(
                     "received: " + JSON.stringify(response.data, null, 4)
                 );
-                // const { data } = response;
 
-                // listing.id = this.setState((prevState) => ({
-                //     listing: response.data.listing,
-                // }));
+                this.setState((prevState) => ({
+                    listing: data.listing,
+                }));
             })
             .catch((error) => {
                 console.log("Error while fetcing listing: " + error);
             });
-
-        this.state.listing.owner = this.context.user.id;
     }
+
+    contactHost = (change) => {
+        change.preventDefault();
+    };
 
     render() {
         const listing = this.state.listing;
 
         return (
             <main role="main">
-                <div className="container">
+                <div className="container-lg">
                     <h1>{listing.title}</h1>
                     <p>
                         <BiStar className="align-middle" />
                         <span className="align-middle ml-1">
                             {listing.rating}
                             <span className="text-muted ml-1">
-                                ({listing.reviews.length})
+                                {/* ({listing.reviews.length}) */}5
                             </span>
                         </span>{" "}
                         <span className="align-middle ml-1 mr-2">·</span>
@@ -154,18 +162,102 @@ class ListingPage extends Component {
                             {listing.address.country}
                         </span>
                     </p>
-                </div>
 
-                {/* <Photos photos={listing.photos} /> */}
+                    {/* <Photos photos={listing.photos} /> */}
 
-                <div className="row">
-                    <div className="col-8 red-border">a</div>
-                    <div className="col-4 blue-border" b></div>
+                    <div className="row">
+                        <div className="col-md-8">
+                            <div className="card border-0">
+                                <Summary
+                                    propertyType={listing.propertyType}
+                                    guests={listing.guests}
+                                    space={listing.space}
+                                    owner={listing.owner}
+                                />
+
+                                <Description
+                                    description={listing.description}
+                                    contactHost={this.constactHost}
+                                />
+
+                                <hr className="my-5" />
+
+                                <BedsAndRooms />
+
+                                <hr className="my-5" />
+                            </div>
+                        </div>
+                        <div className="col-md-4">b</div>
+                    </div>
                 </div>
             </main>
         );
     }
 }
+
+const Description = ({ description, contactHost }) => {
+    return (
+        <div className="card-body p-0">
+            <p className="card-text">{description}</p>
+
+            <button className="btn btn-outline-secondary" onClick={contactHost}>
+                Contact host
+            </button>
+        </div>
+    );
+};
+
+const Summary = ({ propertyType, guests, space, owner }) => {
+    return (
+        <div className="card-header p-0">
+            <div className="row no-gutters">
+                <div className="col">
+                    <h2 className="card-title mb-1">
+                        <span>{propertyType}</span>
+                        <span className="w-100 d-none d-md-block"></span>
+                        <span>hosted by {owner.firstName}</span>
+                    </h2>
+                    <p>
+                        <span>
+                            {guests} guest
+                            {guests === 1 ? "" : "s"}
+                        </span>
+                        <span className="mx-2">·</span>
+                        <span>
+                            {space.beds} bed
+                            {space.beds === 1 ? "" : "s"}
+                        </span>
+                        <span className="mx-2">·</span>
+                        <span>
+                            {space.bedrooms} bedroom
+                            {space.bedrooms === 1 ? "" : "s"}
+                        </span>
+                        <span className="mx-2">·</span>
+                        <span>
+                            {space.bathrooms} bathroom
+                            {space.bathrooms === 1 ? "" : "s"}
+                        </span>
+                    </p>
+                </div>
+                <div className="col-auto mr-2  align-self-center">
+                    <img
+                        src={owner.photo}
+                        className="rounded-circle img-circle-60"
+                        alt=""
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const BedsAndRooms = ({}) => {
+    return (
+        <div className="card-body p-0">
+            <p>test</p>
+        </div>
+    );
+};
 
 const Photos = ({ photos }) => {
     return (
