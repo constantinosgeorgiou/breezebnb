b14dbf706cc30a7aff7ac80bec85f9d764b0acf3
leonidasefrem <leonidasefrem@gmail.com>
2020-09-25 23:19:21 +0300

    update readme and main photo

--- views/src/pages/HomePage.js
+++ views/src/pages/HomePage.js
@@ -2,56 +2,23 @@ import React from "react";
 import { Jumbotron, Container } from "react-bootstrap";
 
 import SearchBar from "../_components/SearchBar";
+import indexphoto from "../assets/imgs/index.jpg";
 
 const HomePage = () => {
     return (
         <main role="main">
+
             <Jumbotron>
                 <Container>
                     <SearchBar />
                 </Container>
             </Jumbotron>
             <Container>
-                <p>
-                    Ipsum minim ullamco ipsum elit anim do. Nisi aliqua ea ex
-                    labore velit. Magna laboris sit velit eu non cupidatat elit
-                    esse ipsum est. Non incididunt reprehenderit nostrud eu ex.
-                    Voluptate aliqua eu eu veniam cupidatat ex. Cupidatat cillum
-                    ea nulla eu mollit occaecat eu. Pariatur sunt amet aliqua
-                    mollit laborum id cillum.
-                </p>
-                <p>
-                    Ipsum minim ullamco ipsum elit anim do. Nisi aliqua ea ex
-                    labore velit. Magna laboris sit velit eu non cupidatat elit
-                    esse ipsum est. Non incididunt reprehenderit nostrud eu ex.
-                    Voluptate aliqua eu eu veniam cupidatat ex. Cupidatat cillum
-                    ea nulla eu mollit occaecat eu. Pariatur sunt amet aliqua
-                    mollit laborum id cillum.
-                </p>
-                <p>
-                    Ipsum minim ullamco ipsum elit anim do. Nisi aliqua ea ex
-                    labore velit. Magna laboris sit velit eu non cupidatat elit
-                    esse ipsum est. Non incididunt reprehenderit nostrud eu ex.
-                    Voluptate aliqua eu eu veniam cupidatat ex. Cupidatat cillum
-                    ea nulla eu mollit occaecat eu. Pariatur sunt amet aliqua
-                    mollit laborum id cillum.
-                </p>
-                <p>
-                    Ipsum minim ullamco ipsum elit anim do. Nisi aliqua ea ex
-                    labore velit. Magna laboris sit velit eu non cupidatat elit
-                    esse ipsum est. Non incididunt reprehenderit nostrud eu ex.
-                    Voluptate aliqua eu eu veniam cupidatat ex. Cupidatat cillum
-                    ea nulla eu mollit occaecat eu. Pariatur sunt amet aliqua
-                    mollit laborum id cillum.
-                </p>
-                <p>
-                    Ipsum minim ullamco ipsum elit anim do. Nisi aliqua ea ex
-                    labore velit. Magna laboris sit velit eu non cupidatat elit
-                    esse ipsum est. Non incididunt reprehenderit nostrud eu ex.
-                    Voluptate aliqua eu eu veniam cupidatat ex. Cupidatat cillum
-                    ea nulla eu mollit occaecat eu. Pariatur sunt amet aliqua
-                    mollit laborum id cillum.
-                </p>
+            <img
+                                    src={indexphoto}
+                                    className="card-img img-sign-in"
+                                    alt=""
+                                ></img>
             </Container>
         </main>
     );
