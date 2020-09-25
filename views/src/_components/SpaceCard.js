1b8299252b2e0368bea7730c81378502c87201bc
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-25 18:18:12 +0300

    add SpaceCard for ListingPage

---
+++ views/src/_components/SpaceCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SpaceCard = ({ name, quantity, icon }) => {
+    return (
+        <div className="card mb-4">
+            <div className="card-body">
+                <div className="row justify-content-center">
+                    {icon}
+                </div>
+                <div className="row justify-content-center">
+                    <h5 className="card-title mb-0">
+                        {name}
+                    </h5>
+                </div>
+                <div className="row justify-content-center">
+                    <p className="font-weight-normal mb-0">
+                        {quantity}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default SpaceCard;
