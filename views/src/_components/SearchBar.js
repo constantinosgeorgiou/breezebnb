82ceb1d271c1f59e7db29c116d6fd2bd44bc86ea
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-25 23:11:27 +0300

    add search

--- views/src/_components/SearchBar.js
+++ views/src/_components/SearchBar.js
@@ -90,11 +90,11 @@ class SearchBar extends Component {
     componentDidMount() {
         getLocations()
             .then(({ data }) => {
-                const { locations } = data;
-                console.log("loc: ", locations);
+                // const { locations } = data;
+
                 this.setState((prevState) => ({
                     isLoading: false,
-                    locations,
+                    locations: data.locations,
                 }));
             })
             .catch((error) => {
@@ -134,12 +134,43 @@ class SearchBar extends Component {
     };
 
     handleSubmit = (event) => {
+        event.preventDefault();
+        const {
+            location,
+            checkInDate,
+            checkOutDate,
+        } = this.state;
+
+        let wrongFormatCheckIn = checkInDate.split(
+            "-"
+        );
+
+        let wrongFormatCheckOut = checkOutDate.split(
+            "-"
+        );
+
+        let parsedLocation = JSON.parse(
+            this.state.location
+        );
+
+        let checkIn = [
+            wrongFormatCheckIn[2],
+            wrongFormatCheckIn[1],
+            wrongFormatCheckIn[0],
+        ].join("/");
+
+        let checkout = [
+            wrongFormatCheckOut[2],
+            wrongFormatCheckOut[1],
+            wrongFormatCheckOut[0],
+        ].join("/");
+
         const parameters = {
-            checkIn: this.state.checkInDate,
-            checkout: this.state.checkOutDate,
-            country: this.state.location.country,
-            state: this.state.location.state,
-            city: this.state.location.city,
+            checkIn: checkIn,
+            checkout: checkout,
+            country: parsedLocation.country,
+            state: parsedLocation.state,
+            city: parsedLocation.city,
         };
 
         //Retrieve available listings locations
@@ -166,207 +197,6 @@ class SearchBar extends Component {
                         error
                 );
             });
-
-        this.setState({
-            listings: [
-                {
-                    id: 1,
-                    title: "Cozy aparetment",
-                    description:
-                        "Sint nulla ex tempor voluptate quis nostrud. Aute ea ipsum ullamco ut do consectetur do. Laboris reprehenderit dolor Lorem quis. Pariatur consectetur enim ex veniam aliqua nulla cillum. Excepteur elit fugiat elit adipisicing cupidatat est consequat et. Culpa velit incididunt sunt id veniam deserunt irure Lorem deserunt fugiat ipsum Lorem. Cillum fugiat minim velit dolore est.",
-                    cost: "10",
-                    type: "Apartment",
-                    rating: 5,
-                    guests: 3,
-                    pictures: [
-                        {
-                            id: 0,
-                            url:
-                                "https://a0.muscache.com/im/pictures/26f549d7-4a09-4faa-b1e4-71fcd1f3a611.jpg?im_w=720",
-                        },
-                        {
-                            id: 1,
-                            url:
-                                "https://a0.muscache.com/im/pictures/af40aa25-8309-414d-b584-600ff0fb8393.jpg?im_w=720",
-                        },
-                        {
-                            id: 2,
-                            url:
-                                "https://a0.muscache.com/im/pictures/e9ebee08-6956-40c2-a1bf-34350ea51b3b.jpg?im_w=720",
-                        },
-                    ],
-                    address: {
-                        country: "Greece",
-                        state: "Athens",
-                        city: "Panormou",
-                        zipCode: 12345,
-                        street: "Papantreou 10",
-                        apartmentNumber: "B1",
-                    },
-                    space: {
-                        beds: 1,
-                        bedrooms: 1,
-                        bathrooms: 1,
-                        livingrooms: 1,
-                        kitchens: true,
-                        rooms: 4,
-                        squareMeters: 100,
-                    },
-                    amenities: {
-                        wifi: true,
-                        shampoo: true,
-                        heating: true,
-                        airConditioning: true,
-                        washer: true,
-                        dryer: true,
-                        breakfast: true,
-                        indoorFireplace: true,
-                        hangers: true,
-                        iron: true,
-                        hairDryer: true,
-                        laptopFriendlyWorkspace: true,
-                        tv: true,
-                        crib: true,
-                    },
-                    rules: {
-                        pets: true,
-                        smoking: true,
-                        events: true,
-                    },
-                },
-                {
-                    id: 2,
-                    title: "Cozy aparetment",
-                    description:
-                        "Sint nulla ex tempor voluptate quis nostrud. Aute ea ipsum ullamco ut do consectetur do. Laboris reprehenderit dolor Lorem quis. Pariatur consectetur enim ex veniam aliqua nulla cillum. Excepteur elit fugiat elit adipisicing cupidatat est consequat et. Culpa velit incididunt sunt id veniam deserunt irure Lorem deserunt fugiat ipsum Lorem. Cillum fugiat minim velit dolore est.",
-                    cost: "10",
-                    type: "Apartment",
-                    rating: 5,
-                    guests: 3,
-
-                    pictures: [
-                        {
-                            id: 0,
-                            url:
-                                "https://a0.muscache.com/im/pictures/26f549d7-4a09-4faa-b1e4-71fcd1f3a611.jpg?im_w=720",
-                        },
-                        {
-                            id: 1,
-                            url:
-                                "https://a0.muscache.com/im/pictures/af40aa25-8309-414d-b584-600ff0fb8393.jpg?im_w=720",
-                        },
-                        {
-                            id: 2,
-                            url:
-                                "https://a0.muscache.com/im/pictures/e9ebee08-6956-40c2-a1bf-34350ea51b3b.jpg?im_w=720",
-                        },
-                    ],
-                    address: {
-                        country: "Greece",
-                        state: "Athens",
-                        city: "Panormou",
-                        zipCode: 12345,
-                        street: "Papantreou 10",
-                        apartmentNumber: "B1",
-                    },
-                    space: {
-                        beds: 1,
-                        bedrooms: 1,
-                        bathrooms: 1,
-                        livingrooms: 1,
-                        kitchens: true,
-                        rooms: 4,
-                        squareMeters: 100,
-                    },
-                    amenities: {
-                        wifi: true,
-                        shampoo: true,
-                        heating: true,
-                        airConditioning: true,
-                        washer: true,
-                        dryer: true,
-                        breakfast: true,
-                        indoorFireplace: true,
-                        hangers: true,
-                        iron: true,
-                        hairDryer: true,
-                        laptopFriendlyWorkspace: true,
-                        tv: true,
-                        crib: true,
-                    },
-                    rules: {
-                        pets: true,
-                        smoking: true,
-                        events: true,
-                    },
-                },
-                {
-                    id: 3,
-                    title: "Cozy aparetment",
-                    description:
-                        "Sint nulla ex tempor voluptate quis nostrud. Aute ea ipsum ullamco ut do consectetur do. Laboris reprehenderit dolor Lorem quis. Pariatur consectetur enim ex veniam aliqua nulla cillum. Excepteur elit fugiat elit adipisicing cupidatat est consequat et. Culpa velit incididunt sunt id veniam deserunt irure Lorem deserunt fugiat ipsum Lorem. Cillum fugiat minim velit dolore est.",
-                    cost: "10",
-                    type: "Apartment",
-                    rating: 5,
-                    guests: 1,
-                    pictures: [
-                        {
-                            id: 0,
-                            url:
-                                "https://a0.muscache.com/im/pictures/26f549d7-4a09-4faa-b1e4-71fcd1f3a611.jpg?im_w=720",
-                        },
-                        {
-                            id: 1,
-                            url:
-                                "https://a0.muscache.com/im/pictures/af40aa25-8309-414d-b584-600ff0fb8393.jpg?im_w=720",
-                        },
-                        {
-                            id: 2,
-                            url:
-                                "https://a0.muscache.com/im/pictures/e9ebee08-6956-40c2-a1bf-34350ea51b3b.jpg?im_w=720",
-                        },
-                    ],
-                    address: {
-                        country: "Greece",
-                        state: "Athens",
-                        city: "Panormou",
-                        zipCode: 12345,
-                        street: "Papantreou 10",
-                        apartmentNumber: "B1",
-                    },
-                    space: {
-                        beds: 1,
-                        bedrooms: 1,
-                        bathrooms: 1,
-                        livingrooms: 1,
-                        kitchens: true,
-                        rooms: 4,
-                        squareMeters: 100,
-                    },
-                    amenities: {
-                        wifi: true,
-                        shampoo: true,
-                        heating: true,
-                        airConditioning: true,
-                        washer: true,
-                        dryer: true,
-                        breakfast: true,
-                        indoorFireplace: true,
-                        hangers: true,
-                        iron: true,
-                        hairDryer: true,
-                        laptopFriendlyWorkspace: true,
-                        tv: true,
-                        crib: true,
-                    },
-                    rules: {
-                        pets: true,
-                        smoking: true,
-                        events: true,
-                    },
-                },
-            ],
-        });
     };
 
     render() {
@@ -386,6 +216,9 @@ class SearchBar extends Component {
             //     // TODO: Add spinner
             //     return <div>Loading...</div>;
             // } else {
+
+            console.log("state: ", this.state);
+
             return (
                 <div className="container-sm">
                     {isDesktop ? (
@@ -415,12 +248,44 @@ class SearchBar extends Component {
                                                     id="inputLocation"
                                                     className="form-control"
                                                     name="location"
-                                                    // value={}
-                                                    // onChange={}
+                                                    value={
+                                                        this
+                                                            .state
+                                                            .location
+                                                    }
+                                                    onChange={
+                                                        this
+                                                            .handleChange
+                                                    }
                                                 >
-                                                    <option value="">
-                                                        Test
-                                                    </option>
+                                                    {this.state.locations.map(
+                                                        (
+                                                            location
+                                                        ) => (
+                                                            <option
+                                                                key={
+                                                                    location.id
+                                                                }
+                                                                value={JSON.stringify(
+                                                                    location
+                                                                )}
+                                                            >
+                                                                {
+                                                                    location.city
+                                                                }
+
+                                                                ,{" "}
+                                                                {
+                                                                    location.state
+                                                                }
+
+                                                                ,{" "}
+                                                                {
+                                                                    location.country
+                                                                }
+                                                            </option>
+                                                        )
+                                                    )}
                                                 </select>
                                             </div>
                                         </div>
@@ -554,12 +419,44 @@ class SearchBar extends Component {
                                                     id="inputLocation"
                                                     className="form-control"
                                                     name="location"
-                                                    // value={}
-                                                    // onChange={}
+                                                    value={
+                                                        this
+                                                            .state
+                                                            .location
+                                                    }
+                                                    onChange={
+                                                        this
+                                                            .handleChange
+                                                    }
                                                 >
-                                                    <option value="">
-                                                        Test
-                                                    </option>
+                                                    {this.state.locations.map(
+                                                        (
+                                                            location
+                                                        ) => (
+                                                            <option
+                                                                key={
+                                                                    location.id
+                                                                }
+                                                                value={JSON.stringify(
+                                                                    location
+                                                                )}
+                                                            >
+                                                                {
+                                                                    location.city
+                                                                }
+
+                                                                ,{" "}
+                                                                {
+                                                                    location.state
+                                                                }
+
+                                                                ,{" "}
+                                                                {
+                                                                    location.country
+                                                                }
+                                                            </option>
+                                                        )
+                                                    )}
                                                 </select>
                                             </div>
                                         </div>
