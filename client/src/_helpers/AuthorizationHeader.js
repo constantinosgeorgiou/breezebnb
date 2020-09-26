b434cd2eba137161e0b80eafdf1d09241fe5d3b0
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-26 19:54:45 +0300

    add server branch

--- client/src/_helpers/AuthorizationHeader.js
+++
@@ -1,9 +0,0 @@
-export default function authorizationHeader() {
-    const user = JSON.parse(localStorage.getItem("user"));
-
-    if (user && user.accessToken) {
-        return { Authorization: "Bearer " + user.accessToken };
-    } else {
-        return {};
-    }
-}
--- client/src/pages/ApplyForHosting.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ApplyForHosting = () => {
-    return <h1>Apply for hosting</h1>;
-};
-
-export default ApplyForHosting;
