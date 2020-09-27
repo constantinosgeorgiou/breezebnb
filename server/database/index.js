cd55f238315e4aacc6aa9a47ee10dd47c636b852
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-09-27 23:00:27 +0300

    change DATABASE_URI to DATABASE_URL

--- server/database/index.js
+++ server/database/index.js
@@ -1,4 +1,4 @@
 const { Pool } = require("pg");
-const pool = new Pool({ connectionString: process.env.DATABASE_URI });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 module.exports = pool;
