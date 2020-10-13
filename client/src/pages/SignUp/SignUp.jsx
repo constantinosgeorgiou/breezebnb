2fdb32e498308daf47bf97a5e48c9acfc03f9873
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-10-13 16:17:36 +0300

    add user state

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -18,6 +18,7 @@ import AccountDetailsForm from "./components/AccountDetailsForm";
 import AddressForm from "./components/AddressForm";
 import HostingForm from "./components/HostingForm";
 import PersonalInformationForm from "./components/PersonalInformationForm";
+import Review from "./components/Review";
 
 const useStyles = makeStyles((theme) => ({
     avatar: {
@@ -44,18 +45,18 @@ const useStyles = makeStyles((theme) => ({
 
 const steps = ["Personal information", "Address", "Account details", "Hosting", "All good?"];
 
-const getStepContent = (step) => {
+const getStepContent = (step, user, handleChange) => {
     switch (step) {
         case 0:
-            return <PersonalInformationForm />;
+            return <PersonalInformationForm user={user} handleChange={handleChange} />;
         case 1:
-            return <AddressForm />;
+            return <AddressForm user={user} handleChange={handleChange} />;
         case 2:
-            return <AccountDetailsForm />;
+            return <AccountDetailsForm user={user} handleChange={handleChange} />;
         case 3:
-            return <HostingForm />;
+            return <HostingForm user={user} handleChange={handleChange} />;
         case 4:
-            return 5;
+            return <Review user={user} handleChange={handleChange} />;
         default:
             throw new Error("Unknown step");
     }
@@ -66,12 +67,48 @@ const SignUp = (props) => {
 
     const [activeStep, setActiveStep] = useState(0);
 
+    const [user, setUser] = useState({
+        // Personal information
+        firstName: "Jane",
+        lastName: "Doe",
+        birthday: "12 / 08 / 2000",
+        phone: "901 - 123434",
+
+        // Address
+        streetAddress: "Blah 12",
+        city: "Athens",
+        state: "Attiki",
+        postalCode: "54123",
+        country: "Greece",
+
+        // Account details
+        username: "janedoe",
+        email: "jane@doe.com",
+        password: "123456",
+        passwordConfirm: "123456",
+
+        // Hosting
+        hosting: false,
+    });
+
     const handleNext = () => {
         setActiveStep(activeStep + 1);
     };
 
     const handleBack = () => {
         setActiveStep(activeStep - 1);
+    };
+
+    const handleChange = (prop) => (event) => {
+        const {
+            target: { type, value },
+        } = event;
+
+        if (type === "checkbox") {
+            setUser({ ...user, hosting: !user.hosting });
+        } else {
+            setUser({ ...user, [prop]: value });
+        }
     };
 
     return (
@@ -91,7 +128,7 @@ const SignUp = (props) => {
                     ))}
                 </Stepper>
                 {/* Render the form of each step */}
-                {getStepContent(activeStep)}
+                {getStepContent(activeStep, user, handleChange)}
 
                 <div className={classes.buttons}>
                     {activeStep !== 0 && (
