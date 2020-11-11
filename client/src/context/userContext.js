adabab0c4891f1674fe5124b7b9d4e2fd76daf8a
Constantinos Georgiou <constantgeorgiou@gmail.com>
2020-11-11 22:38:41 +0200

    add faker

--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,29 +1,53 @@
-import React, { Component, createContext } from 'react'
+import React, { Component, createContext } from "react";
 
-import { getCurrentUser } from 'services/users'
+import Faker from "faker";
 
-const { Provider, Consumer } = createContext()
+// import { getCurrentUser } from "services/users";
+
+const { Provider, Consumer } = createContext();
 
 class UserContextProvider extends Component {
     state = {
-        user: getCurrentUser() || null,
-    }
+        // user: getCurrentUser() || null,
+        user: {
+            id: Faker.random.uuid(),
+            joined: Faker.date.past(),
+            about: Faker.lorem.paragraphs(3),
+            userName: Faker.internet.userName(),
+            firstName: Faker.name.firstName(),
+            lastName: Faker.name.lastName(),
+            email: Faker.internet.email(),
+            phone: Faker.phone.phoneNumber(),
+            isHost: false,
+            isAdmin: false,
+            avatar: Faker.internet.avatar(),
+            birthday: Faker.date.past(),
+            address: {
+                country: Faker.address.country(),
+                state: Faker.address.state(),
+                zipCode: Faker.address.zipCode(),
+                city: Faker.address.city(),
+                streetAddress: Faker.address.streetAddress(),
+            },
+        },
+        // user: null,
+    };
 
     removeUser = () => {
-        this.setState(prevState => {
+        this.setState((prevState) => {
             return {
-                user: null
-            }
-        })
-    }
+                user: null,
+            };
+        });
+    };
 
     render() {
         return (
             <Provider value={{ user: this.state.user, removeUser: this.removeUser }}>
                 {this.props.children}
             </Provider>
-        )
+        );
     }
 }
 
-export { UserContextProvider, Consumer as UserContextConsumer }
+export { Provider, UserContextProvider, Consumer as UserContextConsumer };
